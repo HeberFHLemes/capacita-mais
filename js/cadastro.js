1992b0b7350903bc8190cfe2dfebfd576fc3f4ea
HeberFHLemes <hfhlemes@gmail.com>
2025-11-06 15:21:06 -0300

    feature: adicionando página de login e outra de cadastro de cursos para administradores do website. processos simulados -> nao ha autenticacao real nem persistencia dos dados cadastrados, apenas telas desenvolvidas!

---
+++ js/cadastro.js
@@ -0,0 +1,10 @@
+document.getElementById("form-curso").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const titulo = document.getElementById("titulo").value;
+    const msg = document.getElementById("msg");
+    msg.textContent = `Curso "${titulo}" cadastrado com sucesso!`;
+    msg.classList.remove("d-none");
+
+    this.reset(); // Limpa os campos
+});
