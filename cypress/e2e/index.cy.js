c681e9294967756c25fd6337304c3c831146fb42
HeberFHLemes <hfhlemes@gmail.com>
2026-03-12 12:53:14 -0300

    tests: adicionando dependencia do Cypress e os dois primeiros testes e2e (páginas index e sobre)

---
+++ cypress/e2e/index.cy.js
@@ -0,0 +1,31 @@
+describe("Tela inicial", () => {
+
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
+  it("carrega estrutura principal", () => {
+
+    cy.get("nav")
+      .should("exist")
+    
+    cy.get("header.hero-section")
+      .should("exist")
+    
+    cy.get("footer")
+      .should("exist")
+
+  })
+  
+  it("redireciona para cursos", () => {
+
+    cy.get('[data-cy="btn-explorar-cursos"]')
+      .should("be.visible")
+      .click()
+
+    cy.location("pathname")
+      .should("eq", "/cursos.html")
+
+  })
+
+})
---
+++ cypress/e2e/sobre.cy.js
@@ -0,0 +1,23 @@
+describe("Tela Sobre Nós", () => {
+
+    beforeEach(() => {
+        cy.visit("/sobre.html")
+    })
+    
+    it("carrega estrutura principal", () => {
+        
+        cy.get("nav")
+            .should("exist")
+
+        cy.get("footer")
+            .should("exist")
+        
+        cy.get("#sobre-section")
+            .should("exist")
+
+        cy.get("#contato-section")
+            .should("exist")
+        
+    })
+
+})
