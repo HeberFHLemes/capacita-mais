8b1b4eb84b33694ca4b1f00d2070202ad990ab6b
HeberFHLemes <hfhlemes@gmail.com>
2026-03-17 16:58:19 -0300

    feat: adicionando o script PHP para inserir usuário no banco de dados, com as variáveis de ambiente definidas

---
+++ scripts/seed_usuario.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Cadastrar usuário (admin) por script, com variáveis de ambiente.
+ * USO (Banco de dados já deve estar funcionando):
+ * - docker compose exec [serviço do php] php scripts/seed_usuario.php
+ * - php scripts/seed_usuario.php
+ */ 
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Database\Conexao;
+
+$conexao = Conexao::getInstance();
+
+$email = getenv('USUARIO_EMAIL');
+
+// Verifica se já existe
+$stmt = $conexao->prepare("SELECT id FROM usuarios WHERE email = :email");
+$stmt->execute([':email' => $email]);
+
+if ($stmt->fetch()) {
+    echo "Usuário já existe.\n";
+    exit;
+}
+
+$senha = password_hash(getenv('USUARIO_SENHA'), PASSWORD_DEFAULT);
+
+$stmt = $conexao->prepare("
+    INSERT INTO usuarios (email, senha)
+    VALUES (:email, :senha)
+");
+
+$sucesso = $stmt->execute([
+    ':email' => $email,
+    ':senha' => $senha
+]);
+
+if ($sucesso) {
+    echo "Usuário criado com sucesso!\n";
+} else {
+    echo "Não foi possível criar usuário...\n";
+}
