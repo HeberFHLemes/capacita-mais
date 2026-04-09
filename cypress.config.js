50dcf04e66725a4e5ece0d84a36e61e19fc9c6ba
HeberFHLemes <hfhlemes@gmail.com>
2026-04-09 15:01:18 -0300

    chore: adicionando license no package.json e composer.json, versão atualizada para 2.0.0

--- cypress.config.js
+++ cypress.config.js
@@ -5,9 +5,6 @@ module.exports = defineConfig({
 
   e2e: {
     baseUrl: "http://localhost:8080",
-    // video: true,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 });
