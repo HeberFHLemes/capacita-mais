55157f2f5718c1e249fac90013c1a72be7bf27e4
HeberFHLemes <hfhlemes@gmail.com>
2026-05-27 20:47:08 -0300

    fix: removendo feature de plataformas no backend

--- backend/src/Utils/Normalizador.php
+++ backend/src/Utils/Normalizador.php
@@ -8,7 +8,7 @@ class Normalizador
      * Torna uma string minúscula, sem acentos e apenas com caracteres alfanuméricos.
      * 
      * Função crucial para garantir a consistência ao cadastrar e editar cursos,
-     * por conta do relacionamento com categorias e plataformas.
+     * por conta do relacionamento com categorias.
      * (ex: Programação Back-End e programacao back end devem cair na mesma categoria)
      */
     public static function normalizarTexto(string $texto): string
