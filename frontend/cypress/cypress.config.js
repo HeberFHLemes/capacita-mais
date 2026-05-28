f6a9dd606786381a1e9b9a59edbbb536d5c541d1
HeberFHLemes <hfhlemes@gmail.com>
2026-05-27 22:29:20 -0300

    chore: movendo testes Cypress para o diretório do frontend

--- frontend/cypress/cypress.config.js
+++ frontend/cypress/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "http://localhost:4200",
     setupNodeEvents(on, config) {},
   },
 });
