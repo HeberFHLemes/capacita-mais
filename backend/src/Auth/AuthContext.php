b7ebaa2914d4d5770abaf8b696b8f89cdcde289e
HeberFHLemes <hfhlemes@gmail.com>
2026-06-09 17:01:05 -0300

    feat(backend): implementando o middleware JWT, que popula um AuthContext com o usuário autenticado

---
+++ backend/src/Auth/AuthContext.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Auth;
+
+use App\Auth\Dto\UsuarioAuth;
+
+/**
+ * Contexto de autenticação do usuário, contendo os dados
+ * do usuário autenticado/autorizado pelo middleware JWT,
+ * para serem utilizados por outras partes da aplicação.
+ */
+final class AuthContext
+{
+    private static ?UsuarioAuth $usuario = null;
+
+    public static function setUsuario(UsuarioAuth $usuario): void
+    {
+        self::$usuario = $usuario;
+    }
+
+    public static function getUsuario(): ?UsuarioAuth
+    {
+        return self::$usuario;
+    }
+}
