c9b342111539695ab640c1cac50c4bdf7bd2e1a6
HeberFHLemes <hfhlemes@gmail.com>
2026-06-10 12:06:38 -0300

    refactor(backend): uso de ApiResponse no index e melhor organizacao do mesmo

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,16 +3,13 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\Application;
+use App\Core\ApiResponse;
 
 try {
-    (new Application())->run();
+    $app = new Application();
+    $app->run();
 
-} catch (\Throwable $e) { // para não retornar stacktrace
+} catch (Throwable $e) { // para não retornar stacktrace
     error_log((string) $e);
-
-    http_response_code(500);
-
-    echo json_encode([
-        'erros' => [ 'Erro interno no servidor' ]
-    ]);
+    ApiResponse::erro('Erro interno no servidor');
 }
