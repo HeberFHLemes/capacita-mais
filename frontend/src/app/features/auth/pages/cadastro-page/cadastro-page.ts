ec33b0f8dade69633bea83d7b5e47aeb8ad43862
HeberFHLemes <hfhlemes@gmail.com>
2026-06-12 12:37:51 -0300

    feat(frontend): implementando página de cadastro de usuário - funcionalidades

--- frontend/src/app/features/auth/pages/cadastro-page/cadastro-page.ts
+++ frontend/src/app/features/auth/pages/cadastro-page/cadastro-page.ts
@@ -1,15 +1,85 @@
-import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Component, inject} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
+import {AuthService} from '../../services/auth-service';
 
 @Component({
   selector: 'app-cadastro-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './cadastro-page.html',
   styleUrl: './cadastro-page.css',
 })
 export class CadastroPage {
+
+  private verificarSeSenhasCoincidem: ValidatorFn =
+    (control: AbstractControl): ValidationErrors | null => {
+
+      const senha = control.get('senha')?.value;
+      const senhaConfirma = control.get('senhaConfirma')?.value;
+
+      return senha === senhaConfirma
+        ? null
+        : { notSame: true };
+    };
+
+  cadastroForm = new FormGroup({
+    email: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.email
+      ]
+    }),
+    nome: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required
+      ]
+    }),
+    senha: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required
+      ]
+    }),
+    senhaConfirma: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required
+      ]
+    })
+  }, { validators: this.verificarSeSenhasCoincidem });
+
+  private readonly authService: AuthService = inject(AuthService);
+
+  private readonly router: Router = inject(Router);
+
   realizarCadastro() {
-    // TODO
+    if (this.cadastroForm.invalid) {
+      this.cadastroForm.markAllAsTouched();
+      return;
+    }
+
+    const dados = this.cadastroForm.getRawValue();
+
+    this.authService
+      .cadastrar(dados)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      });
   }
 }
