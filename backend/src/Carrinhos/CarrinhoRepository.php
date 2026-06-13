1d77afe5d66854c6243e993658b5d8502f7d9b8c
HeberFHLemes <hfhlemes@gmail.com>
2026-06-13 16:40:22 -0300

    feat(backend): implementando camada de acesso aos dados dos carrinhos de compra

---
+++ backend/src/Carrinhos/CarrinhoRepository.php
@@ -0,0 +1,142 @@
+<?php declare(strict_types=1);
+
+namespace App\Carrinhos;
+
+use App\Carrinhos\Exceptions\ItemCarrinhoJaExisteException;
+use App\Carrinhos\Exceptions\ItemCarrinhoNaoEncontradoException;
+
+use PDO;
+use PDOException;
+
+readonly class CarrinhoRepository
+{
+    public function __construct(private PDO $conexao) {}
+
+    public function buscarCarrinhoPorUsuario(int $usuarioId): Carrinho
+    {
+        // Realizando duas consultas separadas:
+        // uma pro conjunto de itens, outra pro total.
+        $itens = $this->buscarItens($usuarioId);
+        $total = $this->buscarTotal($usuarioId);
+
+        return new Carrinho($total, $itens);
+    }
+
+    public function buscarTotal(int $usuarioId): float
+    {
+        $sql = "SELECT 
+                COALESCE(SUM(c.preco), 0) AS total 
+                FROM itens_carrinho ic
+                JOIN cursos c ON c.id = ic.curso_id
+                WHERE ic.usuario_id = :usuarioId
+        ";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute([':usuarioId' => $usuarioId]);
+
+        $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return (float) $dados['total'];
+    }
+
+    /**
+     * @param int $usuarioId
+     * @return ItemCarrinho[]
+     */
+    public function buscarItens(int $usuarioId): array
+    {
+        $sql = "SELECT 
+                c.id, 
+                c.nome, 
+                c.preco
+            FROM itens_carrinho ic
+            JOIN cursos c ON c.id = ic.curso_id
+            WHERE ic.usuario_id = :usuarioId
+        ";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute([':usuarioId' => $usuarioId]);
+
+        $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $itens = [];
+
+        foreach ($dados as $row) {
+            $itens[] = new ItemCarrinho(
+                (int) $row['id'],
+                $row['nome'],
+                (float) $row['preco']
+            );
+        }
+
+        return $itens;
+    }
+
+    public function buscarItem(int $usuarioId, int $cursoId): ItemCarrinho
+    {
+        $sql = "SELECT 
+                c.id, 
+                c.nome, 
+                c.preco
+            FROM itens_carrinho ic
+            JOIN cursos c ON c.id = ic.curso_id
+            WHERE ic.usuario_id = :usuarioId AND ic.curso_id = :cursoId
+        ";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute([
+            ':usuarioId' => $usuarioId,
+            ':cursoId' => $cursoId
+        ]);
+
+        $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$dados) {
+            throw new ItemCarrinhoNaoEncontradoException();
+        }
+
+        return new ItemCarrinho(
+            (int) $dados['id'],
+            $dados['nome'],
+            (float) $dados['preco']
+        );
+    }
+
+    public function inserirItem(int $usuarioId, int $cursoId): void
+    {
+        $sql = "INSERT INTO itens_carrinho (usuario_id, curso_id) 
+                VALUES (:usuarioId, :cursoId)
+        ";
+
+        $stmt = $this->conexao->prepare($sql);
+
+        try {
+            $stmt->execute([
+                ':usuarioId' => $usuarioId,
+                ':cursoId' => $cursoId
+            ]);
+        } catch (PDOException $e) {
+            // tratar erro de campos UNIQUE.
+            if ($e->errorInfo[1] === 1062) {
+                throw new ItemCarrinhoJaExisteException();
+            }
+            throw $e;
+        }
+        // diferente dos outros repositories da aplicação,
+        // por ser chave conjunta não se retorna o lastInsertId aqui
+        // (já se conhece os ids para busca).
+    }
+
+    public function removerItem(int $usuarioId, int $cursoId): bool
+    {
+        $sql = "DELETE FROM itens_carrinho WHERE curso_id = :cursoId AND usuario_id = :usuarioId";
+        $stmt = $this->conexao->prepare($sql);
+
+        $stmt->execute([
+            ':usuarioId' => $usuarioId,
+            ':cursoId' => $cursoId
+        ]);
+
+        return $stmt->rowCount() > 0;
+    }
+}
