e1d7a89aeaba91df30a7eb4645515fbb5e0aacff
HeberFHLemes <hfhlemes@gmail.com>
2026-06-12 21:04:39 -0300

    feat(frontend): implementando a navbar de acordo com o perfil. Algumas opções ainda serão implementadas.

--- frontend/src/app/shared/components/navbar/navbar.ts
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,5 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../features/auth/services/auth-service';
+
+interface NavItem {
+  label: string;
+  rota?: string;
+  acao?: 'logout' | 'abrirCarrinho';
+  icone?: string;
+}
+
+enum IconeNavbar {
+  HOME = 'bi bi-house',
+  LOGIN = 'bi bi-box-arrow-in-right',
+  CARRINHO = 'bi bi-cart3',
+  CURSOS = 'bi bi-mortarboard',
+  ADMIN = 'bi bi-person-gear',
+  LOGOUT = 'bi bi-box-arrow-right',
+}
 
 @Component({
   selector: 'app-navbar',
@@ -10,4 +27,79 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class Navbar {
 
+  readonly authService: AuthService = inject(AuthService);
+  readonly router: Router = inject(Router);
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+
+  executarAcao(acao: 'logout'|'abrirCarrinho'): void {
+    if (acao === 'logout') {
+      this.logout();
+    } else {
+      this.abrirCarrinho();
+    }
+  }
+
+  abrirCarrinho(): void {
+    // TODO: abrir aside do carrinho de compras do usuário
+  }
+
+  readonly itensNavbar = computed<NavItem[]>(() => {
+    let navItems: NavItem[] = [];
+
+    navItems.push(
+      {
+        label: 'Início',
+        rota: '/',
+        icone: IconeNavbar.HOME
+      }
+    );
+
+    if (!this.authService.isUsuarioAutenticado()) {
+      navItems.push(
+        {
+          label: 'Entrar',
+          rota: '/login',
+          icone: IconeNavbar.LOGIN
+        }
+      );
+      return navItems;
+    }
+
+    if (this.authService.isAdmin()) {
+      navItems.push(
+        {
+          label: 'Área administrativa',
+          rota: '/admin',
+          icone: IconeNavbar.ADMIN
+        }
+      )
+    } else {
+      navItems.push(
+        {
+          label: 'Meus cursos',
+          rota: '/meus-cursos',
+          icone: IconeNavbar.CURSOS
+        },
+        {
+          label: 'Meu carrinho',
+          acao: 'abrirCarrinho',
+          icone: IconeNavbar.CARRINHO
+        },
+      );
+    }
+
+    navItems.push(
+      {
+        label: 'Sair',
+        acao: 'logout',
+        icone: IconeNavbar.LOGOUT
+      }
+    );
+
+    return navItems;
+  });
 }
