af83e197bdd4327ffbbaf36877bda024d4e6b733
HeberFHLemes <hfhlemes@gmail.com>
2026-06-16 01:57:56 -0300

    feat(frontend): implementando o componente do carrinho de compras e adicionando ao layout

---
+++ frontend/src/app/features/carrinhos/components/botao-adicionar-ao-carrinho/botao-adicionar-ao-carrinho.ts
@@ -0,0 +1,33 @@
+import { Component, inject, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CarrinhoApiService } from '../../services/carrinho-api-service';
+import { AuthService } from '../../../auth/services/auth-service';
+import {CarrinhoService } from '../../services/carrinho-service';
+
+@Component({
+  selector: 'app-botao-adicionar-ao-carrinho',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './botao-adicionar-ao-carrinho.html',
+  styleUrl: './botao-adicionar-ao-carrinho.css',
+})
+export class BotaoAdicionarAoCarrinho {
+
+  isUsuarioAutenticado: boolean = inject(AuthService).isUsuarioAutenticado();
+
+  @Input({ required: true })
+  cursoId!: number;
+
+  readonly carrinhoApiService: CarrinhoApiService = inject(CarrinhoApiService);
+  readonly carrinhoService: CarrinhoService = inject(CarrinhoService);
+
+  adicionarAoCarrinho(): void {
+    this.carrinhoApiService.inserirItemNoCarrinho(this.cursoId).subscribe(
+      ()=>{
+        this.carrinhoService.abrir();
+      }
+    );
+  }
+}
