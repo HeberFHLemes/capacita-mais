794740c173ce140dfa92ac6f795f29937e69d993
HeberFHLemes <hfhlemes@gmail.com>
2026-06-19 02:02:40 -0300

    refactor(frontend): exportando formulário de curso para componente próprio a ser reutilizado na página de edição e cadastro

--- frontend/src/app/features/admin/pages/cadastro-curso-page/cadastro-curso-page.ts
+++ frontend/src/app/features/admin/pages/cadastro-curso-page/cadastro-curso-page.ts
@@ -1,17 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { AdminCursosApiService } from '../../services/admin-cursos-api-service';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CategoriaApiService } from '../../../categorias/services/categoria-api-service';
 import { Categoria } from '../../../categorias/models/categoria';
 import { CursoRequest } from '../../models/curso-request';
-import { Alerta } from '../../../../shared/components/alerta/alerta';
 import { HttpErrorResponse } from '@angular/common/http';
+import { CursoForm } from '../../components/curso-form/curso-form';
 
 @Component({
   selector: 'app-cadastro-curso-page',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, Alerta],
+  imports: [ReactiveFormsModule, CursoForm],
   templateUrl: './cadastro-curso-page.html',
   styleUrl: './cadastro-curso-page.css',
 })
@@ -26,33 +25,8 @@ export class CadastroCursoPage implements OnInit {
   mensagem: string|null = null;
   tipoMensagem: 'success'|'warning'|'danger'|null = null;
 
-  cadastroCursoForm = new FormGroup({
-    nome: new FormControl<string>('', {
-      nonNullable: true,
-      validators: [ Validators.required ]
-    }),
-    descricao: new FormControl<string>(''),
-    categoria: new FormControl<number>(0, { // id, mas começa no disabled "selecione a categoria"
-      nonNullable: true,
-      validators: [ Validators.required, Validators.min(1) ]
-    }),
-    nivel: new FormControl<'iniciante'|'intermediario'|'avancado'>('iniciante', {
-      nonNullable: true,
-      validators: [ Validators.required ]
-    }),
-    preco: new FormControl<number>(0, {
-      nonNullable: true,
-      validators: [ Validators.required, Validators.min(0) ]
-    }),
-    preco_original: new FormControl<number>(0, {
-      nonNullable: true,
-      validators: [ Validators.required, Validators.min(0) ]
-    }),
-    em_destaque: new FormControl<boolean>(false, {
-      nonNullable: true,
-      validators: [ Validators.required ]
-    })
-  });
+  @ViewChild(CursoForm)
+  cursoForm!: CursoForm;
 
   ngOnInit(): void {
     this.carregarCategorias();
@@ -63,38 +37,13 @@ export class CadastroCursoPage implements OnInit {
       .subscribe((resposta) => this.categorias = resposta);
   }
 
-  cadastrarCurso(): void {
-
-    if (this.cadastroCursoForm.invalid) {
-      this.mensagem = 'Existem campos com valores inválidos';
-      this.tipoMensagem = 'warning';
-      return;
-    }
-
-    let form = this.cadastroCursoForm.getRawValue();
-
-    if (form.preco > form.preco_original) {
-      this.mensagem = 'Preço deve ser menor ou igual ao preço original';
-      this.tipoMensagem = 'warning';
-      return;
-    }
-
+  cadastrarCurso(curso: CursoRequest): void {
     this.limparMensagem();
-
-    let curso: CursoRequest = {
-      nome: form.nome,
-      descricao: form.descricao,
-      categoria_id: form.categoria,
-      nivel: form.nivel,
-      em_destaque: form.em_destaque,
-      preco: form.preco,
-      preco_original: form.preco_original,
-    }
 
     this.cursosApiService.cadastrarCurso(curso)
       .subscribe({
         next: (resposta) => {
-          this.cadastroCursoForm.reset();
+          this.cursoForm.resetar();
 
           this.tipoMensagem = 'success';
           this.mensagem = 'Curso cadastrado com sucesso!';
